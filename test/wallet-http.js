981d774499bfb627464461867c4080c709f62c38
Mark Tyneway <mark@purse.io>
2019-05-17 11:29:47 -0700

    test: wallet/http create secondary wallet for redeem

--- test/wallet-http.js
+++ test/wallet-http.js
@@ -35,6 +35,7 @@ const wclient = new WalletClient({
 });
 
 const wallet = wclient.wallet('primary');
+const wallet2 = wclient.wallet('secondary');
 
 let name, cbAddress;
 const accountTwo = 'foobar';
@@ -47,6 +48,7 @@ describe('Wallet HTTP', function() {
     await nclient.open();
     await wclient.open();
 
+    await wclient.createWallet('secondary');
     cbAddress = (await wallet.createAddress('default')).address;
     await wallet.createAccount(accountTwo);
   });
@@ -73,6 +75,14 @@ describe('Wallet HTTP', function() {
 
     const info = await nclient.getInfo();
     assert.equal(info.chain.height, height);
+  });
+
+  it('should mine to the secondary/default wallet', async () => {
+    const {address} = await wallet2.createAddress('default');
+    for (let i = 0; i < 5; i++)
+      await nclient.execute('generatetoaddress', [1, address]);
+
+    assert.ok(true);
   });
 
   it('should have no name state indexed', async () => {
