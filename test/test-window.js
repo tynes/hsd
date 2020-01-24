48ad0c7ecd4211134bb735928d26798552c97306
Javed Khan <tuxcanfly@gmail.com>
2020-01-24 16:26:03 +0530

    getproof: integrate slidingwindow

--- test/test-window.js
+++ test/test-window.js
@@ -4,7 +4,7 @@
 'use strict';
 
 const assert = require('bsert');
-const SlidingWindow = require('./window');
+const SlidingWindow = require('../lib/net/window');
 
 async function sleep(time) {
   return new Promise(resolve => setTimeout(resolve, time));
@@ -18,7 +18,6 @@ describe('SlidingWindow (Unit)', function() {
   });
 
   beforeEach(() => {
-    window.reset();
     window.start();
   });
 
@@ -28,22 +27,23 @@ describe('SlidingWindow (Unit)', function() {
   });
 
   it('should process max requests in window', async () => {
-    for (let i=0; i < window.limit; i++)
-      assert.doesNotThrow(() => window.get());
+    for (let i=0; i < window.max; i++)
+      assert.doesNotThrow(() => window.increase(1));
   });
 
-  it('should throw after max requests in window', async () => {
-    for (let i=0; i < window.limit; i++)
-      window.get();
+  it('should reject after max requests in window', async () => {
+    for (let i=0; i < window.max; i++)
+      window.increase(1);
 
-    assert.throws(() => window.get());
+    window.increase(1);
+    assert.ok(!window.allow());
   });
 
   it('should reset after window timeout', async () => {
     let reset = false;
 
-    for (let i=0; i < window.limit; i++)
-      window.get();
+    for (let i=0; i < window.max; i++)
+      window.increase(1);
 
     window.once('reset', () => {
       reset = true;
@@ -57,63 +57,65 @@ describe('SlidingWindow (Unit)', function() {
   it('should process max requests in sliding window', async () => {
     let window1 = 50;
     for (let i=0; i < window1; i++)
-      window.get();
+      window.increase(1);
 
     await sleep(window.window);
 
     let window2 = 50;
     for (let i=0; i < window2; i++)
-      window.get();
+      window.increase(1);
 
     let score = window.score();
     assert.strictEqual(score, window1 + window2);
   });
 
-  it('should throw after max requests in sliding window', async () => {
+  it('should reject after max requests in sliding window', async () => {
     let window1 = 50;
     for (let i=0; i < window1; i++)
-      window.get();
+      window.increase(1);
 
     await sleep(window.window);
 
     let window2 = 50;
     for (let i=0; i < window2; i++)
-      window.get();
+      window.increase(1);
 
-    assert.throws(() => window.get());
+    window.increase(1);
+    assert.ok(!window.allow());
   });
 
   it('should process weighted max requests in sliding window', async () => {
     let window1 = 50;
     for (let i=0; i < window1; i++)
-      window.get();
+      window.increase(1);
 
     await sleep(1.5 * window.window);
 
     let window2 = 50;
     for (let i=0; i < window2; i++)
-      window.get();
+      window.increase(1);
 
     // We sleep for 50%  of the second window (1.5x window)
-    // so score = 50 * 0.5 + 50 = 75
+    // so score = 50 * (1-0.5) + 50 = 75
     let score = window.score();
     assert.strictEqual(score, 75);
   });
 
-  it('should throw after max weighted requests in sliding window', async () => {
+  it('should reject after max weighted requests in sliding window', async () => {
     let window1 = 50;
     for (let i=0; i < window1; i++)
-      window.get();
+      window.increase(1);
 
     await sleep(1.5 * window.window);
 
     let window2 = 50;
     for (let i=0; i < window2; i++)
-      window.get();
+      window.increase(1);
 
     for (let i=0; i < 25; i++)
-      window.get();
+      window.increase(1);
 
-    assert.throws(() => window.get());
+    window.increase(1);
+    assert.ok(!window.allow());
   });
 });
