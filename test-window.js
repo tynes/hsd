2377e1eca769ef9e406693d9d58457c9e879a6e8
Javed Khan <tuxcanfly@gmail.com>
2020-01-24 15:31:23 +0530

    getproof: rewrite tests

--- test-window.js
+++ test-window.js
@@ -4,47 +4,116 @@
 'use strict';
 
 const assert = require('bsert');
-const Window = require('./window');
+const SlidingWindow = require('./window');
 
 async function sleep(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
-describe('Window (Unit)', function() {
+describe('SlidingWindow (Unit)', function() {
 
-  // Sliding window for 100 requests/second
-  const window = new Window({
-      window: 1000,
-      limit: 100
+  const window = new SlidingWindow({
+    window: 10,
+    limit: 100
   });
 
   beforeEach(() => {
     window.reset();
+    window.start();
   });
 
-  it('should limit to 100 requests/second', async () => {
-    for (let i=0; i < 100; i++)
-      window.get();
-
-    assert.throws(window.get, Error, 'over the limit');
+  afterEach(() => {
+    window.reset();
+    window.stop();
   });
 
-  it('should limit to 100 requests/second over a sliding window ', async () => {
-    /*
-     *  < 1s > < 1s >
-     *  -------------
-     *  | 50  | 50  |
-     *  -------------
-     */
+  it('should process max requests in window', async () => {
+    for (let i=0; i < window.limit; i++)
+      assert.doesNotThrow(() => window.get());
+  });
 
-    for (let i=0; i < 50; i++)
+  it('should throw after max requests in window', async () => {
+    for (let i=0; i < window.limit; i++)
       window.get();
 
-    await sleep(window.window); 
+    assert.throws(() => window.get());
+  });
 
-    for (let i=0; i < 50; i++)
+  it('should reset after window timeout', async () => {
+    let reset = false;
+
+    for (let i=0; i < window.limit; i++)
       window.get();
 
-    assert.throws(window.get, Error, 'over the limit');
+    window.once('reset', () => {
+      reset = true;
+    });
+
+    await sleep(window.window);
+
+    assert.ok(reset === true);
+  });
+
+  it('should process max requests in sliding window', async () => {
+    let window1 = 50;
+    for (let i=0; i < window1; i++)
+      window.get();
+
+    await sleep(window.window);
+
+    let window2 = 50;
+    for (let i=0; i < window2; i++)
+      window.get();
+
+    let score = window.score();
+    assert.strictEqual(score, window1 + window2);
+  });
+
+  it('should throw after max requests in sliding window', async () => {
+    let window1 = 50;
+    for (let i=0; i < window1; i++)
+      window.get();
+
+    await sleep(window.window);
+
+    let window2 = 50;
+    for (let i=0; i < window2; i++)
+      window.get();
+
+    assert.throws(() => window.get());
+  });
+
+  it('should process weighted max requests in sliding window', async () => {
+    let window1 = 50;
+    for (let i=0; i < window1; i++)
+      window.get();
+
+    await sleep(1.5 * window.window);
+
+    let window2 = 50;
+    for (let i=0; i < window2; i++)
+      window.get();
+
+    // We sleep for 50%  of the second window (1.5x window)
+    // so score = 50 * 0.5 + 50 = 75
+    let score = window.score();
+    assert.strictEqual(score, 75);
+  });
+
+  it('should throw after max weighted requests in sliding window', async () => {
+    let window1 = 50;
+    for (let i=0; i < window1; i++)
+      window.get();
+
+    await sleep(1.5 * window.window);
+
+    let window2 = 50;
+    for (let i=0; i < window2; i++)
+      window.get();
+
+    for (let i=0; i < 25; i++)
+      window.get();
+
+    assert.throws(() => window.get());
   });
 });
--- window.js
+++ window.js
@@ -1,28 +1,58 @@
 'use strict';
 
-class Window extends Object {
+const assert = require('bsert');
+const AsyncEmitter = require('bevent');
+
+class SlidingWindow extends AsyncEmitter {
   /**
-   * Create a sliding window
-   * @property {Number} window - window period in seconds
-   * @property {Number} limit - max limit allowed
+   * Create a sliding window counter
+   * @property {Number} window - window period in milliseconds
+   * @property {Number} limit - max requests allowed
+   * @property {Timeout} timeout - sliding window timeout
+   * @property {Number} counter - current window counter
+   * @property {Number} previous - previous window counter
+   * @property {Number} current - current window start time in milliseconds
    */
 
   constructor(options) {
-    super(options);
+    super();
 
     this.window = options.window;
     this.limit = options.limit;
+
+    this.timeout = null;
     this.counter = 0;
-    this.timeout = setTimeout(this.reset, this.window);
+    this.previous = 0;
+    this.current = 0;
+  }
+
+  start() {
+    this.current = Date.now();
+    this.timeout = setTimeout(() => this.reset(), this.window);
+  }
+
+  stop() {
+    this.current = 0;
+    clearTimeout(this.timeout);
   }
 
   async reset() {
-    // TODO: lock?
+    this.previous = this.counter;
     this.counter = 0;
+    this.current = Date.now();
+    this.emit('reset');
+  }
+
+  score() {
+    const ms = Date.now() - this.current;
+    const percent = 1 - (ms / this.window);
+    assert(percent > 0, 'invalid previous window weight');
+
+    return this.previous * percent + this.counter;
   }
 
   get() {
-    if (this.counter > this.limit)
+    if (this.score() >= this.limit)
       throw Error('over the limit');
 
     this.counter++;
@@ -30,4 +60,4 @@ class Window extends Object {
   }
 }
 
-module.exports = Window;
+module.exports = SlidingWindow;
